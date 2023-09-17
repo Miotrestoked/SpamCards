2dcd958fc2f53a6e96673d79efc67f8a3703e403
Timo <487384@student.saxion.nl>
2023-09-17 14:33:42 +0200

    First commit + first card

---
+++ SpamCards/Cards/SmallTweak.cs
@@ -0,0 +1,71 @@
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace SpamCards.Cards
+{
+    class SmallTweak : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats,
+            CharacterStatModifiers statModifiers, Block block)
+        {
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data,
+            HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Edits values on player when card is selected
+            gun.damage = 1f + (15f / gun.damage);
+        }
+
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data,
+            HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            //Run when the card is removed from the player
+        }
+
+        protected override string GetTitle()
+        {
+            return "Small tweak";
+        }
+
+        protected override string GetDescription()
+        {
+            return "A small, flat boost to your damage.";
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Common;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new CardInfoStat[]
+            {
+                new CardInfoStat()
+                {
+                    positive = true,
+                    stat = "Flat damage",
+                    amount = "+15",
+                    simepleAmount = CardInfoStat.SimpleAmount.aLittleBitOf
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.ColdBlue;
+        }
+
+        public override string GetModName()
+        {
+            return SpamCards.ModInitials;
+        }
+    }
+}
---
+++ SpamCards/SpamCards.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using HarmonyLib;
+using SpamCards.Cards;
+using UnboundLib.Cards;
+
+namespace SpamCards
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound")]
+    [BepInDependency("pykess.rounds.plugins.moddingutils")]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch")]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class SpamCards : BaseUnityPlugin
+    {
+        private const string ModId = "turtle.mods.spamcards";
+        private const string ModName = "Spam Cardpack";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "SCP";
+
+        public static SpamCards Instance { get; private set; } = default!;
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+
+        void Start()
+        {
+            Instance = this;
+            CustomCard.BuildCard<SmallTweak>();
+        }
+    }
+}
